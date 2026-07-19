d2589c2b366f83da22b75f353d39c95a5946531d
Ayush Maurya <mauryaayush010907@gmail.com>
2026-07-19 21:49:34 +0530

    Update project

---
+++ next.config.ts
@@ -0,0 +1,18 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.fbcdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "**.cdninstagram.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
